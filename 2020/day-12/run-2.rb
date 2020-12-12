bdde1884e8523b4fcb2cbfcba36b72c5578f4c9c
olliekennedy <ollie.kennedy1@gmail.com>
2020-12-12 06:39:45 +0000

    refactor day 12 a little

--- 2020/day-12/run-2.rb
+++ 2020/day-12/run-2.rb
@@ -1,53 +1,24 @@
-input = File.readlines('./input.txt').map{ |x| x.gsub("\n", "") }.map { |x| x.insert(1,".").split(".")}.map {|x| [x[0], x[1].to_i]}#.map(&:to_i)
-puts input.inspect
+input = File.readlines('./input.txt').map(&:chomp).map { |x| [x[0], x[1..-1].to_i] }
 
-
-
-def correct_degrees
-  if @dir > 360
-    @dir -= 360
-  elsif @dir < 0
-    @dir += 360
-  elsif @dir == 360
-    @dir = 0
+def rotate(dir, degrees)
+  case degrees
+  when 90
+    dir == "R" ? @wayp = [@wayp[1], -@wayp[0]] : rotate("R", 270)
+  when 180
+    @wayp = [-@wayp[0], -@wayp[1]]
+  when 270
+    dir == "R" ? @wayp = [-@wayp[1], @wayp[0]] : rotate("R", 90)
   end
 end
 
 @x = 0
 @y = 0
 @wayp = [10,1]
-
-def move(dist)
-  @x += (@wayp[0] * dist)
-  @y += (@wayp[1] * dist)
-end
-
-def rotate(dir, degrees)
-  if dir == "R"
-    case degrees
-    when 90
-      @wayp = [@wayp[1], -@wayp[0]]
-    when 180
-      @wayp = [-@wayp[0], -@wayp[1]]
-    when 270
-      @wayp = [-@wayp[1], @wayp[0]]
-    end
-  else
-    case degrees
-    when 90
-      @wayp = [-@wayp[1], @wayp[0]]
-    when 180
-      @wayp = [-@wayp[0], -@wayp[1]]
-    when 270
-      @wayp = [@wayp[1], -@wayp[0]]
-    end
-  end
-end
-
 input.each do |x|
   case x[0]
   when "F"
-    move(x[1])
+    @x += (@wayp[0] * x[1])
+    @y += (@wayp[1] * x[1])
   when "N"
     @wayp[1] += x[1]
   when "E"
@@ -56,15 +27,9 @@ input.each do |x|
     @wayp[1] -= x[1]
   when "W"
     @wayp[0] -= x[1]
-  when "L"
-    rotate("L", x[1])
-  when "R"
-    rotate("R", x[1])
+  when "L", "R"
+    rotate(x[0], x[1])
   end
 end
 
-
-
-puts "x = #{@x}"
-puts "y = #{@y}"
 puts @x.abs + @y.abs
