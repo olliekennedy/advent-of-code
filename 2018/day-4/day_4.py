5806c522b83cd06466a3e722120703263b87b456
Oliver Kennedy <ollie.kennedy1@gmail.com>
2022-08-02 19:52:14 +0100

    another attempt at 2021 day 16 (wip) and an ugly solution to 2018 day 4

---
+++ 2018/day-4/day_4.py
@@ -0,0 +1,90 @@
+def main():
+    file_contents = open('input.txt').read()
+
+    formatted_log = sorted_formatted_log(file_contents)
+    guards_sleep_log = per_guard_sleep_log_from(formatted_log)
+    guards_minute_log = convert_to_log_of_minutes_from(guards_sleep_log)
+    guards_total_minutes = total_minutes_per_guard_from(guards_minute_log)
+
+    sleepiest_guard = max(guards_total_minutes, key=guards_total_minutes.get)
+    sleepiest_minute = max(guards_minute_log[sleepiest_guard], key=guards_minute_log[sleepiest_guard].get)
+    part_1_answer = int(sleepiest_guard) * int(sleepiest_minute)
+    print('part 1 answer:', part_1_answer)
+    assert part_1_answer == 39698
+
+    maxes = find_max_minutes(guards_minute_log)
+    part_2_answer = int(maxes['guard']) * maxes['minute']
+    print('part 2 answer:', part_2_answer)
+    assert part_2_answer == 14920
+
+
+def find_max_minutes(guards_minute_log):
+    maxes = {
+        'guard': '',
+        'minute': '',
+        'occurrences': 0
+    }
+    for guard, minute_log in guards_minute_log.items():
+        for minute, occurrences in minute_log.items():
+            if occurrences > maxes['occurrences']:
+                maxes['guard'] = guard
+                maxes['minute'] = minute
+                maxes['occurrences'] = occurrences
+    return maxes
+
+
+def total_minutes_per_guard_from(guards_minute_log):
+    guards_total_minutes = {}
+    for guard, minute_log in guards_minute_log.items():
+        guard_total = sum(minute_log.values())
+        guards_total_minutes[guard] = guard_total
+    return guards_total_minutes
+
+
+def convert_to_log_of_minutes_from(guards_sleep_log):
+    guards_minute_log = {}
+    for guard, sleep_times in guards_sleep_log.items():
+        minute_log = {}
+        for sleep_time in sleep_times:
+            for minute in range(int(sleep_time[0][3]), int(sleep_time[1][3])):
+                if minute not in minute_log:
+                    minute_log[minute] = 1
+                    continue
+                minute_log[minute] += 1
+        guards_minute_log[guard] = minute_log
+    return guards_minute_log
+
+
+def per_guard_sleep_log_from(formatted_log):
+    guards_sleep_log = {}
+    current_guard = ''
+    for i in range(len(formatted_log)):
+        log = formatted_log[i]
+        if log[4].isnumeric():
+            if log[4] not in guards_sleep_log:
+                guards_sleep_log[log[4]] = []
+            current_guard = log[4]
+            continue
+        if log[4] == 'asleep':
+            guards_sleep_log[current_guard].append([log[0:4], formatted_log[i + 1][0:4]])
+    return guards_sleep_log
+
+
+def sorted_formatted_log(file_contents):
+    formatted_log = []
+    for line in file_contents.split('\n'):
+        formatted_line = line
+        if 'Guard' in formatted_line:
+            formatted_line = formatted_line.replace('] Guard #', ',').replace(' begins shift', '')
+        if 'asleep' in formatted_line:
+            formatted_line = formatted_line.replace('] falls asleep', ',asleep')
+        if 'up' in formatted_line:
+            formatted_line = formatted_line.replace('] wakes up', ',up')
+        formatted_line = formatted_line.replace('[1518-', '').replace(' ', ',').replace('-', ',').replace(':', ',')
+        formatted_log.append(formatted_line.split(','))
+    formatted_log.sort()
+    return formatted_log
+
+
+if __name__ == '__main__':
+    main()
--- 2021/day-16/day-16.py
+++ 2021/day-16/day-16.py
@@ -1,4 +1,22 @@
 TEST = True
+MAPPING = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111',
+}
 
 
 def read_file(filename):
@@ -7,6 +25,7 @@ def read_file(filename):
 
 def process_literal(binary_number):
     chunk_list = []
+    i = 0
     for i in range(0, len(binary_number), 5):
         chunk_list.append(binary_number[i:i + 5])
         if binary_number[i] == '0':
@@ -14,45 +33,79 @@ def process_literal(binary_number):
 
     result_bin = ''.join([chunk[1:] for chunk in chunk_list])
     result = dec_from_bin(result_bin)
-    print(result)
-    return result
+    print('literal value:', result)
+    remaining = binary_number[i+5:]
+    return remaining
 
 
 def main():
     one_answer = run('one.txt')
     print('one_answer:', one_answer)
-    assert one_answer == 2021
+    # assert one_answer == 2021
     two_answer = run('two.txt')
     print('two_answer:', two_answer)
+    three_answer = run('three.txt')
+    print('three_answer:', three_answer)
 
 
 def process_total_length(binary_number):
-    length_of_all_subpackets = dec_from_bin(binary_number[:15])
+    print('binary_number:', binary_number)
+    binary_total_length = binary_number[0:15]
+    print('binary_total_length:', binary_total_length)
+    length_of_all_subpackets = dec_from_bin(binary_total_length)
     print('length_of_all_subpackets:', length_of_all_subpackets)
+
+    subpacket = binary_number[15:15 + length_of_all_subpackets]
+    process_subpacket(subpacket)
+    remaining = binary_number[15+length_of_all_subpackets:]
+    return remaining
+
 
 
 def process_operator(binary_number):
     length_type_id = binary_number[0]
+    print('binary number before length type id:', binary_number)
     print('length_type_id:', length_type_id)
+    remaining = ''
     if length_type_id == '0':
-        process_total_length(binary_number[1:])
+        remaining = process_total_length(binary_number[1:])
+    elif length_type_id == '1':
+        remaining = process_subpacket(binary_number[1:], 3)
+    else:
+        print('wrong length type id')
+    return remaining
 
 
 def run(filename):
     hex_string = read_file(filename)
     binary_string = binary_string_from_hex_string(hex_string)
+    return process_subpacket(binary_string)
+
+
+def process_subpacket(binary_string, quantity=-1):
+    if quantity == 0:
+        print('finished processing the given number of subpackets')
+        return binary_string
+    if len(binary_string) < 11:
+        print('subpacket too small:', binary_string)
+        return
     packet_version_bin = binary_string[0:3]
     packet_version = dec_from_bin(packet_version_bin)
     type_id_bin = binary_string[3:6]
     type_id = dec_from_bin(type_id_bin)
     binary_number = binary_string[6:]
+    print('binary_string:', binary_string)
     answer = 'placeholder'
     if type_id == 4:
-        answer = process_literal(binary_number)
+        remaining = process_literal(binary_number)
     else:
-        answer = process_operator(binary_number)
+        remaining = process_operator(binary_number)
     print([f'{name} = {value}' for name, value in locals().items()])
-    return answer
+    if quantity == -1:
+        new_quantity = quantity
+    else:
+        new_quantity = quantity - 1
+    return process_subpacket(remaining, new_quantity)
 
 
 def dec_from_bin(packet_version_bin) -> int:
@@ -60,7 +113,7 @@ def dec_from_bin(packet_version_bin) -> int:
 
 
 def binary_string_from_hex_string(file_contents):
-    return bin(int(file_contents, 16))[2:].zfill(8)
+    return ''.join(MAPPING[char] for char in file_contents)
 
 
 if __name__ == '__main__':
